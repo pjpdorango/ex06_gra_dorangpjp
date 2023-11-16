1ba93866864c11a58bf56a72c8eb71517b26e950
L3GenDri <b2026pjpdorango@pshs.edu.ph>
2023-11-17 02:01:18 +0800

    Made monster generation code cleaner and more readable Added comments and moved part of code to battle function

--- src/ex06_gra_dorangopjp/EX06_GRA_DorangoPJP.java
+++ src/ex06_gra_dorangopjp/EX06_GRA_DorangoPJP.java
@@ -27,38 +27,35 @@ public class EX06_GRA_DorangoPJP {
         // 2 vs 0
         for (int i = 0; i < 6; i++) {
             // Generate the two monsters to battle
-            int temp = i % 3;
-            Monster monster1 = (Monster) Monster.getMonsterList().get(temp);
-            int i2 = temp + 1; // Temporary index of monster 2
-            // If i2 is greater than monsterList, set to 0
+            // Since there are only 3 enemies, i1 is 0 - 2 (3 options)
+            // i1 and i2 are temporary indexes of monster 1 and 2 respectively
+            int i1 = i % Monster.getMonsterList().size();
+            int i2 = i1 + 1;
+            
+            // If i2 is greater than monsterList, reset to 0
             if (i2 > Monster.getMonsterList().size() - 1) {
                 i2 = 0;
             }
+            
+            // Set monster1 and monster2 to monster at index i1 and i2 respectively
+            Monster monster1 = (Monster) Monster.getMonsterList().get(i1);
             Monster monster2 = (Monster) Monster.getMonsterList().get(i2);
-            
-            System.out.println("----------------------------------------");
-            System.out.println(monster1.getName() + " will battle against " + monster2.getName() + "!");
-            System.out.println("----------------------------------------");
-            
-            System.out.println(monster1.getName() + " - " + monster1.getHP() + " HP");
-            System.out.println(monster2.getName() + " - " + monster2.getHP() + " HP");
-            
-            // MAKE TWO ENEMIES BATTLE            
+                     
             battle(monster1, monster2);
-            
-            System.out.println(monster1.getName() + " - " + monster1.getHP() + " HP");
-            System.out.println(monster2.getName() + " - " + monster2.getHP() + " HP");
-            
-            monster1.resetHealth();
-            monster2.resetHealth();
-            
-            monster1.resetStats();
-            monster2.resetStats();
         }
     }
     
     public static void battle(Monster monster1, Monster monster2) {
         Random rand = new Random();
+            
+        // BATTLE START
+        System.out.println("----------------------------------------");
+        System.out.println(monster1.getName() + " will battle against " + monster2.getName() + "!");
+        System.out.println("----------------------------------------");
+
+        System.out.println(monster1.getName() + " - " + monster1.getHP() + " HP");
+        System.out.println(monster2.getName() + " - " + monster2.getHP() + " HP");
+            
         int round = 1;
         // Continue while both monsters have hp higher than 0.
         while (monster1.getHP() > 0 && monster2.getHP() > 0) {
@@ -80,7 +77,7 @@ public class EX06_GRA_DorangoPJP {
             // Choosing a random choice (Attack, Guard, Charge, Rest, or Special)
             // Guard, Charge, Rest and Special each have 1/9 chance to occur
             // Attack has 5/9 chance to occur (battles move too slow otherwise)
-            int nextMove = rand.nextInt(5);
+            int nextMove = rand.nextInt(9);
             switch (nextMove) {
                 case 0:
                     monster.attack(enemy);
@@ -104,5 +101,14 @@ public class EX06_GRA_DorangoPJP {
 
             round++;
         }
+        
+        System.out.println(monster1.getName() + " - " + monster1.getHP() + " HP");
+        System.out.println(monster2.getName() + " - " + monster2.getHP() + " HP");
+
+        monster1.resetHealth();
+        monster2.resetHealth();
+
+        monster1.resetStats();
+        monster2.resetStats();
     }
 }
